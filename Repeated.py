569f3323570f95ce56f3412e6c134845268c5450
swethaselvanambi <46991350+swethaselvanambi@users.noreply.github.com>
2019-06-11 10:09:03 +0530

    Update Repeated.py

--- Repeated.py
+++ Repeated.py
@@ -1,10 +1,6 @@
 n=int(input())
-a=list()
-for i in range(0,n):
-    b=input()
-    a.append(b)
-#a=input()
-#a=[int(i) for i in a]
+a=input().split()
+a=[int(i) for i in a]
 #print(a)
 b=list()
 for i in a:
