1996fb1d942bfa385f4c35ea56345251124ddc64
swethaselvanambi <46991350+swethaselvanambi@users.noreply.github.com>
2019-06-11 13:46:39 +0530

    Update SQR.c

--- SQR.c
+++ SQR.c
@@ -5,11 +5,11 @@ int main()
 	for(i=0;i<4;i++)
 	{
 		for(j=0;j<2;j++)
-		scanf("%d",&a[i][j])
+		scanf("%d",&a[i][j]);
 	}
 	if(a[0][0]!=a[1][0]||a[2][0]!=a[3][0]||a[0][1]!=a[2][1]||a[1][1]!=a[3][1])
-	printf("yes");
-	else
 	printf("no");
+	else
+	printf("yes");
 	return 0;
 }
