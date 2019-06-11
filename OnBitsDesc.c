fe4375c98aa042892ea06bf97ea8ab6c6608239c
swethaselvanambi <46991350+swethaselvanambi@users.noreply.github.com>
2019-06-11 11:48:12 +0530

    Create OnBitsDesc.c

---
+++ OnBitsDesc.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int ones(int n)
+{
+	int c=0;
+	unsigned long int m=1,a=1;
+	while(m<n)
+	{
+		if(m&n)
+			c++;
+		m=m<<a;	
+	}
+	return c;
+}
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	unsigned int a[n],b[n],t;
+	for(i=0;i<n;i++)
+		scanf("%u",&a[i]);
+	for(i=0;i<n;i++)
+		b[i]=ones(a[i]);
+	for(i=0;i<n-1;i++)
+		for(j=i+1;j<n;j++)
+			if(b[i]<b[j])
+			{
+				t=b[i];
+				b[i]=b[j];
+				b[j]=t;
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+			else if(b[i]==b[j])
+			{
+				if(a[i]<a[j])
+				{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+				}
+			}
+	for(i=0;i<n;i++)
+	printf("%u\n",a[i]);
+	return 0;
+}
