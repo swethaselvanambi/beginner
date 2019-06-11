7023b81e033e76cc40a9d8684ab5ae6361ec3791
swethaselvanambi <46991350+swethaselvanambi@users.noreply.github.com>
2019-06-11 12:23:54 +0530

    Create xANDxdigitSum.py

---
+++ xANDxdigitSum.py
@@ -0,0 +1,15 @@
+n=int(input())
+c=0
+b=list()
+for i in range(n):
+	s=0
+	a=[int(d) for d in str(i)]
+	for j in a:
+		s+=j
+	if(i+s==n):
+		b.append(i)
+		c++
+print(c)
+if(len(b)):
+	for i in b:
+		print(b)
