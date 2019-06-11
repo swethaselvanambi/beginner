0e2b4dbf79999762153602b56c9099dd5cc23cc4
swethaselvanambi <46991350+swethaselvanambi@users.noreply.github.com>
2019-06-11 09:56:41 +0530

    Create Repeated.py

---
+++ Repeated.py
@@ -0,0 +1,11 @@
+a=input()
+a=[int(i) for i in a]
+#print(a)
+b=list()
+for i in a:
+    if(a.count(i)>1):
+        b.append(int(i))
+b = list(dict.fromkeys(b))
+b.sort()
+for i in b:
+    print(i,end=" ")
